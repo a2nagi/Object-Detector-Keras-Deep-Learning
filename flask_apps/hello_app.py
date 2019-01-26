73a9c8d6560e13ffe38f6751595dc71df3e4a057
Arjan Nagi <a2nagi@uwaterloo.ca>
2019-01-25 23:37:46 -0500

    Add hello_app

--- flask_apps/hello_app.py
+++ flask_apps/hello_app.py
@@ -1,1 +1,12 @@
+from flask import request
+from flask import Flask
+from flask import jsonify
 
+app = Flask(__name__)
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    body = request.get_json(force=True)
+    name = body['name]
+    response = {'greeting': 'Hello, ' + name + '!'}
+    return jsonify(response)
